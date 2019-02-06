7db8c4d7d0cee29c666e7694d39981b6bd04a8d5
Sean Holmes <sean_holmes@discovery.com>
2019-02-06 12:02:59 -0500

    Destroyed VPC

--- resources.tf
+++ resources.tf
@@ -6,11 +6,11 @@
 #    Name = "terraformDosVPC"
 #  }
 #}
-resource "aws_vpc" "VPC2" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tags {
-    Name = "terraformDosVPC"
-  }
-}
+#resource "aws_vpc" "VPC2" {
+#  cidr_block = "10.0.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support = true
+#  tags {
+#    Name = "terraformDosVPC"
+#  }
+#}
