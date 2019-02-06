966abeb6724cb849e0ae258527608f116c8cf5ad
Sean Holmes <sean_holmes@discovery.com>
2019-02-06 11:10:20 -0500

    added tf files

---
+++ resources.tf
@@ -0,0 +1,16 @@
+#resource "aws_vpc" "environment-example-two" {
+#  cidr_block = "10.0.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support = true
+#  tags {
+#    Name = "terraformDosVPC"
+#  }
+#}
+resource "aws_vpc" "VPC2" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "terraformDosVPC"
+  }
+}
